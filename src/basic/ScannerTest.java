5976f462eb799c4018f1862fb2500e0bb5a87a3b
luckykapok918 <luckykapok918@163.com>
2015-07-17 16:07:38 +0800

    新增文件

---
+++ src/basic/ScannerTest.java
@@ -0,0 +1,14 @@
+package basic;
+import java.util.Scanner; 
+public class ScannerTest {
+
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int a, b;
+		a = cin.nextInt();
+		b = cin.nextInt();
+		System.out.println(a + b);
+
+	}
+
+}
